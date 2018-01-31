dea4c6a05572f931f0024b2c03b443e7a7550e2f
vesche <austinjackson892@gmail.com>
2018-01-30 22:58:56 -0500

    fix color mappings

--- board.py
+++ board.py
@@ -1,13 +1,11 @@
-
-#
-# blocks.py
-#
+# -*- coding: utf-8 -*-
 
 from random import randint
 
-COLORS = ['white', 'grey', 'red', 'green', 'yellow', 'orange', 'tan', 'pink']
+COLORS = ['white', 'grey', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan']
 ROWS = 5
 COLS = 10
+
 
 def checker(color_a, color_b):
     board = []
@@ -20,11 +18,13 @@ def checker(color_a, color_b):
             board += [code_a, code_b][::-1]*(COLS//2)
     return board
 
+
 def standard():
     board = []
     for i in range(2, ROWS+2):
         board += [i]*COLS
     return board
+
 
 def random():
     board = []
